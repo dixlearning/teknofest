6bcf106856784c6eaeba7a7f70081a8d40422f67
izustarseed <starseed2024@hotmail.com>
2024-08-28 21:50:15 +0300

    Update disleksi2.dart

--- dixlearning/lib/etkinlik_sorulari/disleksi2.dart
+++ dixlearning/lib/etkinlik_sorulari/disleksi2.dart
@@ -10,37 +10,36 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final List<String> words = [
     '',
-    'bitki',
-    'börek',
-    'değirmen',
-    'peynir',
-    'dünya',
-    'boncuk',
-    'papatya',
-    'bardak',
-    'baston',
-    'balon',
-    'palto',
-    'bilye',
-    'pamuk',
-    'defter',
-    'priz',
-    'başlık',
-    'bütün',
-    'balık',
+    'Bitki',
+    'Börek',
+    'Değirmen',
+    'Peynir',
+    'Dünya',
+    'Boncuk',
+    'Papatya',
+    'Bardak',
+    'Baston',
+    'Balon',
+    'Palto',
+    'Bilye',
+    'Pamuk',
+    'Defter',
+    'Priz',
+    'Başlık',
+    'Bütün',
+    'Balık',
     ''
   ];
 
   final List<bool> isCorrect = List<bool>.filled(20, false);
   final List<bool> isWrong = List<bool>.filled(20, false);
   final List<bool> isNext = List<bool>.filled(20, false);
-  int currentIndex = 1; // Başlangıçta sadece bitki seçilebilir
+  int currentIndex = 1;
 
   @override
   void initState() {
     super.initState();
-    _highlightNextWords(
-        start: true); // Oyunun başlangıcında sadece "bitki" seçilebilir
+    _highlightNextWords(start: true);
   }
 
   void _highlightNextWords({bool start = false}) {
@@ -49,7 +48,6 @@ class _HomeScreenState extends State<HomeScreen> {
         isNext[i] = false;
       }
 
-      // Başlangıçta sadece bitki seçilebilir
       if (start) {
         isNext[currentIndex] = true;
       } else {
@@ -70,9 +68,106 @@ class _HomeScreenState extends State<HomeScreen> {
         isWrong[i] = false;
       }
       currentIndex = 1;
-      _highlightNextWords(
-          start: true); // Yeniden başlarken sadece "bitki" seçilebilir
+      _highlightNextWords(start: true);
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.lightGreen[300],
+      appBar: AppBar(
+        backgroundColor: Colors.lightGreen[600],
+        title: const Text('Tavuğu Yeme Götürelim!'),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            color: Colors.lightGreen[200],
+            child: const Text(
+              '\n"B" harfiyle başlayan kelimeleri takip edelim ve tavuğu yeme götürelim!\n\n"Bitki" kutucuğuna tıklayarak başlayabilirsin!\n',
+              style: TextStyle(fontSize: 18),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    childAspectRatio: 1.5,
+                  ),
+                  shrinkWrap: true,
+                  itemCount: words.length,
+                  itemBuilder: (context, index) {
+                    if (index == 0) {
+                      return Container(
+                        margin: const EdgeInsets.all(5),
+                        color: Colors.grey[300],
+                        child: Image.asset(
+                            'assets/images/disleksi2_img/tavuk1.jpg'),
+                      );
+                    } else if (index == 19) {
+                      return Container(
+                        margin: const EdgeInsets.all(2),
+                        color: Colors.grey[300],
+                        child:
+                            Image.asset('assets/images/disleksi2_img/yem.jpg'),
+                      );
+                    } else {
+                      return GestureDetector(
+                        onTap: isNext[index]
+                            ? () {
+                                setState(() {
+                                  if (words[index]
+                                      .toLowerCase()
+                                      .startsWith('b')) {
+                                    isCorrect[index] = true;
+                                    isWrong[index] = false;
+                                    currentIndex = index;
+                                    _highlightNextWords();
+                                  } else {
+                                    isWrong[index] = true;
+                                    isCorrect[index] = false;
+                                    _showTryAgainDialog();
+                                  }
+
+                                  if (words[index].toLowerCase() == 'balık') {
+                                    _showEndDialog();
+                                  }
+                                });
+                              }
+                            : null,
+                        child: Container(
+                          margin: const EdgeInsets.all(2),
+                          color: isCorrect[index]
+                              ? Colors.lightGreen
+                              : isWrong[index]
+                                  ? Colors.red
+                                  : isNext[index]
+                                      ? Colors.blue
+                                      : Colors.grey[300],
+                          child: Center(
+                            child: Text(
+                              words[index],
+                              textAlign: TextAlign.center,
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                          ),
+                        ),
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   void _showEndDialog() {
@@ -80,8 +175,8 @@ class _HomeScreenState extends State<HomeScreen> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('BAŞARDINIZ!'),
-          content: const Text('Tebrikler, oyunu başarıyla tamamladınız.'),
+          title: const Text('BAŞARDIN!'),
+          content: const Text('Tebrikler, oyunu başarıyla tamamladın!'),
           actions: [
             TextButton(
               child: const Text('Tamam'),
@@ -96,111 +191,13 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor:
-          Colors.lightGreen[300], // Arka plan rengini açık yeşil yaptım
-      appBar: AppBar(
-        backgroundColor: Colors
-            .lightGreen[600], // Başlığın arka plan rengini koyu yeşil yaptım
-        title: const Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Tavuğu Yeme Götür'),
-            SizedBox(height: 1), // Metin ile başlık arasında biraz boşluk bırak
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 4.0),
-              child: Text(
-                '"b" harfiyle başlayan kelimeleri takip ederek tavuğu yeme götürün.',
-                style: TextStyle(fontSize: 12), // Yazı boyutunu biraz küçülttüm
-                textAlign: TextAlign.center,
-                softWrap:
-                    true, // Metnin ekranın genişliğine göre sarılmasını sağlar
-              ),
-            ),
-          ],
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: GridView.builder(
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              childAspectRatio: 1.5,
-            ),
-            itemCount: words.length,
-            itemBuilder: (context, index) {
-              if (index == 0) {
-                return Container(
-                  margin: const EdgeInsets.all(5),
-                  color: Colors.grey[300],
-                  child: Image.asset(
-                      'assets/images/disleksi2_img/tavuk1.jpg'), // Tavuk resmi
-                );
-              } else if (index == 19) {
-                return Container(
-                  margin: const EdgeInsets.all(2),
-                  color: Colors.grey[300],
-                  child: Image.asset(
-                      'assets/images/disleksi2_img/yem.jpg'), // Yem resmi
-                );
-              } else {
-                return GestureDetector(
-                  onTap: isNext[index]
-                      ? () {
-                          setState(() {
-                            if (words[index].startsWith('b')) {
-                              isCorrect[index] = true;
-                              isWrong[index] = false;
-                              currentIndex = index;
-                              _highlightNextWords();
-                            } else {
-                              isWrong[index] = true;
-                              isCorrect[index] = false;
-                              _showTryAgainDialog();
-                            }
-
-                            if (words[index] == 'balık') {
-                              _showEndDialog();
-                            }
-                          });
-                        }
-                      : null,
-                  child: Container(
-                    margin: const EdgeInsets.all(2),
-                    color: isCorrect[index]
-                        ? Colors.lightGreen
-                        : isWrong[index]
-                            ? Colors.red
-                            : isNext[index]
-                                ? Colors.blue
-                                : Colors.grey[300],
-                    child: Center(
-                      child: Text(
-                        words[index],
-                        textAlign: TextAlign.center,
-                        style: const TextStyle(fontSize: 14),
-                      ),
-                    ),
-                  ),
-                );
-              }
-            },
-          ),
-        ),
-      ),
-    );
-  }
-
   void _showTryAgainDialog() {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('YANLIŞ SEÇİM!'),
-          content: const Text('Tekrar deneyin.'),
+          title: const Text('Üzgünüm, yanlış seçim!'),
+          content: const Text('Hadi tekrar deneyelim!'),
           actions: [
             TextButton(
               child: const Text('Tamam'),
