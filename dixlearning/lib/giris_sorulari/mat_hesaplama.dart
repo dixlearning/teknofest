a2af8b776838792a270d1294d5a626fe18f9f379
izustarseed <starseed2024@hotmail.com>
2024-08-29 19:43:04 +0300

    Update mat_hesaplama.dart

--- dixlearning/lib/giris_sorulari/mat_hesaplama.dart
+++ dixlearning/lib/giris_sorulari/mat_hesaplama.dart
@@ -20,9 +20,8 @@ class _HomePageState extends State<HomePage2> {
   List<int> results = [3, 5, 2, 6, 4];
   List<bool> matched = List.filled(5, false);
   List<Color> equationColors =
-      List.filled(5, const Color.fromARGB(255, 223, 4, 4));
-  List<Color> resultColors =
-      List.filled(5, const Color.fromARGB(255, 225, 7, 7));
+      List.filled(5, const Color.fromARGB(255, 255, 255, 255));
+  List<Color> resultColors = List.filled(5, Colors.black);
   List<bool> disabled = List.filled(5, false);
 
   int correctCount = 0;
@@ -33,92 +32,146 @@ class _HomePageState extends State<HomePage2> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Matematik Oyunu'),
+        title: const Text('Hoş Geldin Testi!'),
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: GridView.builder(
-              padding: const EdgeInsets.all(16.0),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            const Text(
+              'Tahtanın üzerinde bulunan işlemleri yapalım!',
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w600,
+                color: Colors.black,
               ),
-              itemCount: equations.length,
-              itemBuilder: (context, index) {
-                return DragTarget<int>(
-                  builder: (
-                    BuildContext context,
-                    List<int?> accepted,
-                    List<dynamic> rejected,
-                  ) {
-                    return Container(
-                      decoration: const BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage(
-                              'assets/images/mat_hesaplama_img/tahta.png'),
-                          fit: BoxFit.cover,
+              textAlign: TextAlign.justify,
+            ),
+            const SizedBox(height: 20),
+            Expanded(
+              child: GridView.builder(
+                padding: const EdgeInsets.symmetric(vertical: 10),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: equations.length,
+                itemBuilder: (context, index) {
+                  return DragTarget<int>(
+                    builder: (
+                      BuildContext context,
+                      List<int?> accepted,
+                      List<dynamic> rejected,
+                    ) {
+                      return Container(
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                'assets/images/mat_hesaplama_img/tahta.png'),
+                            fit: BoxFit.cover,
+                          ),
                         ),
+                        child: Center(
+                          child: Text(
+                            equations[index]['equation'],
+                            style: TextStyle(
+                              color: equationColors[index],
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              decoration: disabled[index]
+                                  ? TextDecoration.lineThrough
+                                  : TextDecoration.none,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        height: 50,
+                        width: 100,
+                      );
+                    },
+                    onWillAcceptWithDetails: (details) {
+                      return !disabled[index];
+                    },
+                    onAcceptWithDetails: (details) {
+                      int receivedData = details.data as int;
+                      if (!disabled[index]) {
+                        setState(() {
+                          if (receivedData == equations[index]['result']) {
+                            matched[index] = true;
+                            equationColors[index] = Colors.green;
+                            correctCount++;
+                          } else {
+                            equationColors[index] = Colors.red;
+                            wrongCount++;
+                          }
+                          disabled[index] = true;
+                          checkGameEnd();
+                        });
+                      }
+                    },
+                  );
+                },
+              ),
+            ),
+            Container(
+              height: 100,
+              color: Colors.grey.shade300,
+              child: GridView.builder(
+                padding: const EdgeInsets.all(16.0),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 5,
+                  childAspectRatio: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                ),
+                itemCount: results.length,
+                itemBuilder: (context, index) {
+                  return Draggable<int>(
+                    data: results[index],
+                    feedback: Material(
+                      color: Colors.transparent,
+                      child: Container(
+                        decoration: const BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage(
+                                'assets/images/mat_hesaplama_img/box.png'),
+                            fit: BoxFit.contain,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(
+                            results[index].toString(),
+                            style: const TextStyle(
+                                color: Colors.black,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        height: 50,
+                        width: 100,
                       ),
+                    ),
+                    childWhenDragging: Container(
+                      color: Colors.grey,
                       child: Center(
                         child: Text(
-                          equations[index]['equation'],
-                          style: TextStyle(
-                            color: equationColors[index],
-                            fontSize: 18,
-                            decoration: disabled[index]
-                                ? TextDecoration.lineThrough
-                                : TextDecoration.none,
-                          ),
+                          results[index].toString(),
+                          style:
+                              const TextStyle(color: Colors.red, fontSize: 18),
                         ),
                       ),
                       height: 50,
                       width: 100,
-                    );
-                  },
-                  onWillAcceptWithDetails: (details) {
-                    return !disabled[index];
-                  },
-                  onAcceptWithDetails: (details) {
-                    int receivedData = details.data as int;
-                    if (!disabled[index]) {
+                    ),
+                    feedbackOffset: const Offset(0, -30),
+                    onDragCompleted: () {
                       setState(() {
-                        if (receivedData == equations[index]['result']) {
-                          matched[index] = true;
-                          equationColors[index] = Colors.green;
-                          correctCount++;
-                        } else {
-                          equationColors[index] = Colors.red;
-                          wrongCount++;
-                        }
-                        disabled[index] = true;
-                        checkGameEnd();
+                        resultColors[index] =
+                            const Color.fromARGB(255, 0, 190, 232);
                       });
-                    }
-                  },
-                );
-              },
-            ),
-          ),
-          Container(
-            height: 100,
-            color: const Color.fromARGB(68, 194, 81, 196),
-            child: GridView.builder(
-              padding: const EdgeInsets.all(16.0),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 5,
-                childAspectRatio: 2,
-                crossAxisSpacing: 10,
-                mainAxisSpacing: 10,
-              ),
-              itemCount: results.length,
-              itemBuilder: (context, index) {
-                return Draggable<int>(
-                  data: results[index],
-                  feedback: Material(
-                    color: Colors.transparent,
+                    },
                     child: Container(
                       decoration: const BoxDecoration(
                         image: DecorationImage(
@@ -130,94 +183,61 @@ class _HomePageState extends State<HomePage2> {
                       child: Center(
                         child: Text(
                           results[index].toString(),
-                          style: const TextStyle(
-                              color: Color.fromARGB(255, 176, 25, 25),
-                              fontSize: 18),
+                          style: TextStyle(
+                            color: resultColors[index],
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            decoration: matched.contains(true)
+                                ? TextDecoration.lineThrough
+                                : TextDecoration.none,
+                          ),
                         ),
                       ),
                       height: 50,
                       width: 100,
                     ),
-                  ),
-                  childWhenDragging: Container(
-                    color: Colors.grey,
-                    child: Center(
-                      child: Text(
-                        results[index].toString(),
-                        style: const TextStyle(color: Colors.red, fontSize: 18),
-                      ),
-                    ),
-                    height: 50,
-                    width: 100,
-                  ),
-                  feedbackOffset: const Offset(0, -30),
-                  onDragCompleted: () {
-                    setState(() {
-                      resultColors[index] =
-                          const Color.fromARGB(255, 0, 190, 232);
-                    });
-                  },
-                  child: Container(
-                    decoration: const BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                            'assets/images/mat_hesaplama_img/box.png'),
-                        fit: BoxFit.contain,
-                      ),
-                    ),
-                    child: Center(
-                      child: Text(
-                        results[index].toString(),
-                        style: TextStyle(
-                          color: resultColors[index],
-                          fontSize: 18,
-                          decoration: matched.contains(true)
-                              ? TextDecoration.lineThrough
-                              : TextDecoration.none,
-                        ),
-                      ),
-                    ),
-                    height: 50,
-                    width: 100,
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Visibility(
-            visible: gameEnded,
-            child: Column(
-              children: [
-                Text(
-                  'Toplam Doğru: $correctCount',
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  'Toplam Yanlış: $wrongCount',
-                  style: const TextStyle(
-                      fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ],
+            const SizedBox(height: 20),
+            Visibility(
+              visible: gameEnded,
+              child: Column(
+                children: [
+                  Text(
+                    'Toplam Doğru: $correctCount',
+                    style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                  Text(
+                    'Toplam Yanlış: $wrongCount',
+                    style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          ElevatedButton(
-            onPressed: gameEnded
-                ? null
-                : () {
-                    setState(() {
-                      gameEnded = true;
-                      // Skoru gösterdikten sonra yeni bir oyuna geçiş yap
-                      Future.delayed(const Duration(seconds: 2), () {
-                        _handleGameEnd();
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: gameEnded
+                  ? null
+                  : () {
+                      setState(() {
+                        gameEnded = true;
+                        Future.delayed(const Duration(seconds: 2), () {
+                          _handleGameEnd();
+                        });
                       });
-                    });
-                  },
-            child: const Text('Oyunu Bitir'),
-          ),
-        ],
+                    },
+              child: const Text('Oyunu Bitir'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -242,7 +262,6 @@ class _HomePageState extends State<HomePage2> {
     if (allMatched) {
       setState(() {
         gameEnded = true;
-        // Skoru gösterdikten sonra yeni bir oyuna geçiş yap
         Future.delayed(const Duration(seconds: 2), () {
           _handleGameEnd();
         });
